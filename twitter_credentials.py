8c8d5f0922ad4e561816e08a70c7052455ca4a6b
KiritoKun007 <37584095+KiritoKun007@users.noreply.github.com>
2019-07-05 14:13:58 +0530

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 13:06:31 2019
+
+@author: ashis
+"""
+#Variables that contain users credentials to access twitter API
+
+ACCESS_TOKEN = "705600278850961415-NecUoCpUHqqNJUh0VParibJjYHQH3bz"
+ACCESS_TOKEN_SECRET = "SfERzo80Cr6qn4Bo1xFybOpKlYq33DFKOWZgcPZ1QRpiR"
+CONSUMER_KEY = "rRoS0K9CpZ6EqBqgejmEX9yTA"
+CONSUMER_SECRET = "96gR7nYkHoY8JmOd4kZgGRLT8nWaa3W7UFq1IVtgtI2dxmOKrT"
